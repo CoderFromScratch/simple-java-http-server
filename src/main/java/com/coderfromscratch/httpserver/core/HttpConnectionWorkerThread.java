8ab9df87e519ac117350bf2b72137eefb6ba8cc1
abhishek <abhishek.karkamkar@gmail.com>
2021-05-06 21:29:37 +0530

    used try-with-resources for closing InputStream/IutStream resources

--- src/main/java/com/coderfromscratch/httpserver/core/HttpConnectionWorkerThread.java
+++ src/main/java/com/coderfromscratch/httpserver/core/HttpConnectionWorkerThread.java
@@ -2,7 +2,6 @@ package com.coderfromscratch.httpserver.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,7 +9,7 @@ import java.net.Socket;
 
 public class HttpConnectionWorkerThread extends Thread {
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
-    private Socket socket;
+    private final Socket socket;
 
     public HttpConnectionWorkerThread(Socket socket) {
         this.socket = socket;
@@ -18,14 +17,14 @@ public class HttpConnectionWorkerThread extends Thread {
 
     @Override
     public void run() {
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
 
-        try {
-            inputStream = socket.getInputStream();
-            outputStream = socket.getOutputStream();
+        try(
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream()
+        ) {
 
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+            String html = "<html><head><title>Simple Java HTTP Server</title></head>" +
+                    "<body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
 
             final String CRLF = "\r\n"; // 13, 10
 
@@ -38,26 +37,14 @@ public class HttpConnectionWorkerThread extends Thread {
 
             outputStream.write(response.getBytes());
 
-
-
             LOGGER.info(" * Connection Processing Finished.");
         } catch (IOException e) {
             LOGGER.error("Problem with communication", e);
         } finally {
-            if (inputStream!= null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {}
-            }
-            if (outputStream!=null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {}
-            }
             if (socket!= null) {
                 try {
                     socket.close();
-                } catch (IOException e) {}
+                } catch (IOException ignored) {}
             }
         }
     }
