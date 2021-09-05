03fc417c9d8bbab7617467e5033bd48a200db18e
Steeely Dan <steeelydan@posteo.de>
2021-09-06 01:59:46 +0200

    Remove unused imports

--- src/main/java/com/coderfromscratch/httpserver/core/ServerListenerThread.java
+++ src/main/java/com/coderfromscratch/httpserver/core/ServerListenerThread.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
